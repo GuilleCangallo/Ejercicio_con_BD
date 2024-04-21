61c5ec3ffb742ed2cf57334a2949fdcf6948d859
Guille <guillermo.mooney@escuelacangallo.edu.ar>
2024-04-21 01:37:31 -0300

    First commit

---
+++ constantes.py
@@ -0,0 +1,12 @@
+""" constantes de la app """
+
+DB_HOSTNAME="127.0.0.1"
+DB_USERNAME="root"
+DB_PASSWORD=""
+DB_DATABASE="ventas"
+
+DB_TABLES=[
+    'clientes',
+    'ordenes',
+    'vendedores',
+]
---
+++ test_main.py
@@ -0,0 +1,43 @@
+"""
+    Test unitarios para cada función del main
+
+    Prof. Tute Ávalos
+"""
+# pylint: disable=missing-function-docstring
+import pytest
+import main
+from constantes import DB_TABLES
+
+def test_abrir_conexion_existe():
+    main_attrs = dir(main)
+    assert 'abrir_conexion' in main_attrs
+
+def test_consulta_generica():
+    main_attrs = dir(main)
+    assert 'consulta_generica' in main_attrs
+
+@pytest.fixture
+def conn_fixture():
+    pytest.dbconn = main.abrir_conexion()
+
+# El fixture se puede declarar pasando por parametro una variable
+# con el mismo nombre del fixture ->
+def test_abrir_conexion(conn_fixture):
+    conn = pytest.dbconn
+    assert conn
+
+# O se puede declarar con el decorador @pytest.mark.usefixtures(<fixture>)
+@pytest.mark.usefixtures("conn_fixture")
+@pytest.mark.parametrize('tabla', DB_TABLES)
+def test_chequear_tabla(tabla):
+    resultado = main.consulta_generica(pytest.dbconn, f"SHOW TABLES LIKE '{tabla}'")
+    assert len(resultado) == 1
+
+def pytest_sessionfinish(session, status):
+    # pylint: disable=unused-argument
+    pytest.dbconn.close()
+
+@pytest.mark.usefixtures("conn_fixture")
+def test_listar_clientes_vendor():
+    clientes = main.clientes_vendedor(pytest.dbconn, 2)
+    assert clientes[0][0] == 1
